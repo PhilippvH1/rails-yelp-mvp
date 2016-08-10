56f149071142283db9c6ff6b7b62710baa49a702
Philipp von Hammerstein <p.vonhammerstein@whu.edu>
2016-08-10 16:23:23 +0200

    First version with separated form for reviews

---
+++ db/migrate/20160810094322_add_restaurant_id_to_reviews.rb
@@ -0,0 +1,6 @@
+class AddRestaurantIdToReviews < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :reviews, :restaurant, index: true
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Epicure", category: "french", address: "Rue de 1")
+Restaurant.create(name: "Lalelu", category: "italian", address: "Rue de 2")
+Restaurant.create(name: "Hebbede", category: "japanese", address: "Rue de 3")
+Restaurant.create(name: "Grand Cru", category: "french", address: "Rue de 4")
+Restaurant.create(name: "Quando costa", category: "chinese", address: "Rue de 5")
